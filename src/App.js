8a10c742576af7fc24e7d224db93b1e65af6f936
Kosiso Igbokwe <kosiso@Kosisos-MacBook-Pro.local>
2023-07-08 21:25:31 -0400

    Implemented start screen and data fetching

--- src/App.js
+++ src/App.js
@@ -1,7 +1,69 @@
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import { useEffect, useReducer } from "react";
+import StartScreen from "./StartScreen";
+
+const initialState = {
+  questions: [],
+
+  // "loading" | "error" | "ready" | "active" | "finished"
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFetchFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:9000/questions");
+        const data = await response.json();
+        dispatch({
+          type: "dataReceived",
+          payload: data,
+        });
+      } catch (error) {
+        dispatch({
+          type: "dataFetchFailed",
+        });
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>React Quiz</h1>
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+
+        {status === "error" && <Error />}
+
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+      </Main>
     </div>
   );
 }
--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,34 +1,92 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+const initialState = {
+  count: 0,
+  step: 1,
+};
+function reducer(state, action) {
+  console.log(state, action);
+  switch (action.type) {
+    case "increment":
+      return {
+        ...state,
+        count: state.count + state.step,
+      };
+    case "decrement":
+      return {
+        ...state,
+        count: state.count - state.step,
+      };
+    case "setCount":
+      return {
+        ...state,
+        count: action.payload,
+      };
+    case "setStep":
+      return {
+        ...state,
+        step: action.payload,
+      };
+
+    case "reset":
+      return {
+        ...initialState,
+      };
+
+    default:
+      throw new Error("Unknown action type");
+  }
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  // const [count, setCount] = useState(0);
+  // const [step, setStep] = useState(1);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
+    dispatch({
+      type: "decrement",
+    });
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
   };
 
   const inc = function () {
+    dispatch({
+      type: "increment",
+    });
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    // setCount(Number(e.target.value));
+    dispatch({
+      type: "setCount",
+      payload: Number(e.target.value),
+    });
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
+    dispatch({
+      type: "setStep",
+      payload: Number(e.target.value),
+    });
   };
 
   const reset = function () {
-    setCount(0);
-    setStep(1);
+    dispatch({
+      type: "reset",
+    });
+    // setCount(0);
+    // setStep(1);
   };
 
   return (
